f65381cf9b77d55d6779aa4b3de57deec33575a9
judinilson monchacha <judinilson0000@gmail.com>
2022-06-21 21:17:04 +0400

    TranferFrom approval

--- contracts/TwodeepToken.sol
+++ contracts/TwodeepToken.sol
@@ -17,6 +17,8 @@ contract TwoDeepToken {
 
     // Returns the account balance of another account with address _owner.
     mapping(address => uint256) public balanceOf;
+    // the amount which _spender is still allowed to withdraw from _owner.
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
@@ -34,6 +36,47 @@ contract TwoDeepToken {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    /*
+        APROVE 
+        Allows _spender to withdraw from your account multiple times, up to the _value amount.
+        If this function is called again it overwrites the current allowance with _value.
+    */
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /*
+        TRANSFER FROM
+        Transfers _value amount of tokens from address _from to address _to, 
+        and MUST fire the Transfer event.
+         allow a contract to transfer tokens on your behalf and/or to charge 
+         fees in sub-currencies. The function SHOULD throw unless the _from account
+          has deliberately authorized the sender of the message via some mechanism.
+    */
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
 
         return true;
     }
